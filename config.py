023b487e0481f2467c6dbc92c61c22ace2716457
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-08-17 19:21:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ consumer_key = "6K5eEHLJqIKtp0zNTx8qmBGeg"
 consumer_secret = "K4fy33GpsHurMQEo1PlRaDuH1r7Yvanfy90mSmgzDapDpZBpd0"
 access_token = "1427443084879532035-jbIfZBm5zfTY8V4XM3q5hXLwgTi7BJ"
 access_token_secret = "jHGcI4szvu12m0GogV7nXdSaySnwlFrJ2VG5DisfYqyzq"
-trigger = "triggerword"
+trigger = "wibuword"
 timezone = 7 #change this into your timezone. for example if your timezone is GMT+7 you put 7
