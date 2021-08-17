b5756ccd99851b852b2213585d6b7c10bfd0ee4f
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-08-17 19:19:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-consumer_key = "YOUR API KEY HERE"
-consumer_secret = "YOUR API KEY SECRET HERE"
-access_token = "YOUR ACCESS TOKEN HERE"
-access_token_secret = "YOUR ACCESS TOKEN SECRET HERE"
+consumer_key = "6K5eEHLJqIKtp0zNTx8qmBGeg"
+consumer_secret = "K4fy33GpsHurMQEo1PlRaDuH1r7Yvanfy90mSmgzDapDpZBpd0"
+access_token = "1427443084879532035-jbIfZBm5zfTY8V4XM3q5hXLwgTi7BJ"
+access_token_secret = "jHGcI4szvu12m0GogV7nXdSaySnwlFrJ2VG5DisfYqyzq"
 trigger = "triggerword"
 timezone = 7 #change this into your timezone. for example if your timezone is GMT+7 you put 7
